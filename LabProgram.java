da0c5bb754a210bc1de47a86d36dcd0b3a55d946
Gabriel Ford <gabe@fordltc.net>
2022-01-26 02:15:23 -0500

    new problem

--- LabProgram.java
+++ LabProgram.java
@@ -1,22 +1,38 @@
 import java.util.Scanner;
 
 public class LabProgram {
-    public static double productOfFour(int a, int b, int c, int d) {
-        double ad = a;
-        return (ad * b * c * d);
-    }
-
-    public static double averageOfFour(int a, int b, int c, int d) {
-        return (a + b + c + d) / 4.0;
-    }
-
     public static void main(String[] args) {
-
         Scanner scnr = new Scanner(System.in);
-        int a = scnr.nextInt();
-        int b = scnr.nextInt();
-        int c = scnr.nextInt();
-        int d = scnr.nextInt();
-        System.out.format("%.3f %.3f", productOfFour(a, b, c, d), averageOfFour(a, b, c, d));
+        String var = scnr.nextLine();
+        System.out.println("You entered: " + var);
+        if (var.indexOf("BFF") > 0) {
+            System.out.println("BFF: best friend forever");
+        }
+        if (var.indexOf("IDK") > 0) {
+            System.out.println("IDK: I don't know");
+        }
+        if (var.indexOf("JK") > 0) {
+            System.out.println("JK: just kidding");
+        }
+        if (var.indexOf("TMI") > 0) {
+            System.out.println("TMI: too much information");
+        }
+        if (var.indexOf("TTYL") > 0) {
+            System.out.println("TTYL: talk to you later");
+        }
+
+        // System.out.println("Input an abbreviation:");
+        // if (var.equals("LOL")) {
+        // System.out.println("laughing out loud");
+        // } else if (var.equals("IMHO")) {
+        // System.out.println("in my humble opinion");
+        // } else if (var.equals("TMI")) {
+        // System.out.println("too much information");
+        // } else if (var.equals("BFF")) {
+        // System.out.println("best friends forever");
+        // } else if (var.equals("IDK")) {
+        // System.out.println("I don't know");
+        // } else {
+        // System.out.println("Unknown");
     }
 }
